a4b639bd1de8301f2734a2768cf81642d87b8214
Lautaro Molina <71042127+L-Molina@users.noreply.github.com>
2022-08-03 23:57:10 -0300

    Delete routes directory

--- routes/homeRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { Router } = require("express");
-const home = Router();
-
-home.get("/", (req, res) => {  
-  res.render('form'); 
-});
-
-module.exports = home;
